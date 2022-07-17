5e9cf839ecea088d402b0e7585db106dffb255d8
Asaf <asafucho@gmail.com>
2022-07-17 16:09:46 +0300

    First attempt at MLPRegressor

--- FAsT-Match/main.py
+++ FAsT-Match/main.py
@@ -3,10 +3,12 @@ from CreateSamples import random_template
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
-# import sklearn
 import time
 from os import listdir
 from csv import writer
+
+from sklearn.neural_network import MLPRegressor
+from sklearn.model_selection import train_test_split
 
 
 def example_run(image, template, real_corners):
@@ -63,30 +65,48 @@ def fast_match(iterations, image_directory, features_file, histogram_file):
     print("\n\n-------------------@@@@@ Total time for the images in directory {}: {:.8f} seconds @@@@@-------------------".format(image_directory, time.time() - tic1))
 
 
-def show_features(data_folder):
+def import_data(data_folder):
     # import the data from the csv files
-    ideal_th = np.array([])
-    features = np.zeros((0, 37))
-    print(ideal_th.shape, ideal_th)
-    print(features.shape, features)
+    all_names = np.array([])
+    all_ideal_th = np.array([])
+    all_features = np.zeros((0, 37))
     for file_name in listdir(data_folder):
         if file_name.endswith(".csv") and file_name.startswith("Training_Data"):
-            # names = np.loadtxt(data_folder + "\\" + file_name, dtype='str', delimiter=',', usecols=0)
+            file_names = np.loadtxt(data_folder + "\\" + file_name, dtype='str', delimiter=',', usecols=0)
             file_ideal_th = np.loadtxt(data_folder + "\\" + file_name, dtype='float', delimiter=',', usecols=1)
-            file_features = np.loadtxt(data_folder + "\\" + file_name, dtype='float', delimiter=',', usecols=tuple(range(2, 39)))
-            print(file_name, file_ideal_th.shape, file_features.shape)
-            ideal_th = np.concatenate((ideal_th, file_ideal_th))
-            features = np.concatenate((features, file_features))
-    print(ideal_th.shape, features.shape)
+            file_features = np.loadtxt(data_folder + "\\" + file_name, dtype='float', delimiter=',',
+                                       usecols=tuple(range(2, 39)))
+            print(file_name, file_names.shape, file_ideal_th.shape, file_features.shape)
+            all_names = np.concatenate((all_names, file_names))
+            all_ideal_th = np.concatenate((all_ideal_th, file_ideal_th))
+            all_features = np.concatenate((all_features, file_features))
+    print(all_names.shape, all_ideal_th.shape, all_features.shape)
+    return all_names, all_ideal_th, all_features
 
+
+def show_features(y, x):
     # Graphs: feature effect on ideal_th
     # check how each feature affect the output
     # c = np.linspace(0, 1, features.shape[0])
-    for feature in range(features.shape[1]):
+    for feature in range(x.shape[1]):
         plt.figure()
-        plt.scatter(features[:, feature], ideal_th[:], alpha=0.1)
+        plt.scatter(x[:, feature], y[:], alpha=0.1)
 
     plt.show()
+
+
+def generalize(exact_y):
+    #   TODO: use the histogram data to determine general_y
+    general_y = exact_y
+    return general_y
+
+
+def process(y, x):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1)
+    network = MLPRegressor()  # verbose=True, early_stopping=True)
+    regr = network.fit(x_train, y_train)
+    score = regr.score(x_test, y_test)
+    print("Score:", score)
 
 
 if __name__ == '__main__':
@@ -100,5 +120,14 @@ if __name__ == '__main__':
     # ex_template = cv2.imread(r"TestImages\template.png")
     # ex_real_corners = np.array([[147, 212], [214, 188], [267, 35], [200, 60]])
     # example_run(ex_image, ex_template, ex_real_corners)
+
     # fast_match(templates_per_image, images_folder, features_csv, histogram_csv)
-    show_features(model_data_folder)
+
+    names, ideal_th, features = import_data(model_data_folder)
+    # show_features(ideal_th, features)
+
+    #   ideal_th is exactly the value for the ground truth to proceed to next level, we need to increase it a bit,
+    #   with respect to the histogram values
+    general_th = generalize(ideal_th)
+
+    process(general_th, features)
